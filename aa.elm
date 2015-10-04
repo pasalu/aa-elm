ba5e14e48d1d2b6d35f4bba738876d71f322acc5
Peter Salu <psalua@gmail.com>
2015-10-04 18:53:32 -0500

    Initial Commit.

---
+++ aa.elm
@@ -0,0 +1,12 @@
+{-
+ - Programmer: Peter Salu
+ - Created on: August 6, 2015
+ - Description: Clone of aa the game.
+ - https://play.google.com/store/apps/details?id=com.aa.generaladaptiveapps&hl=en
+ -}
+
+module Aa where
+
+import Graphics.Element exposing (show)
+
+main = show "Hello World"
