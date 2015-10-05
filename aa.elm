9d0ce894677844adf2785bbbd8f2aecf0d5985e5
Peter Salu <psalua@gmail.com>
2015-10-04 19:30:37 -0500

    Added input.

--- aa.elm
+++ aa.elm
@@ -8,5 +8,22 @@
 module Aa where
 
 import Graphics.Element exposing (show)
+import Signal exposing ((<~), (~))
+import Time exposing (Time, fps, inSeconds)
+import Keyboard
 
-main = show "Hello World"
+--Inputs
+type alias Input =
+  { space : Bool
+  , delta : Time
+  }
+
+delta : Signal Time
+delta = inSeconds <~ (fps 60)
+
+input : Signal Input
+input =
+  Signal.sampleOn delta <|
+    Input <~ Keyboard.space ~ delta
+
+main = show <~ input
