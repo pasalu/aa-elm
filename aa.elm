5f629da372f71245d24507a1d44643d89f24f1fe
Peter Salu <psalua@gmail.com>
2015-11-03 21:05:36 -0600

    Added view.

--- aa.elm
+++ aa.elm
@@ -7,11 +7,16 @@
 
 module Aa where
 
-import Graphics.Element exposing (show)
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing
+  (Form, Shape, move, moveY, filled, rect, circle, collage, group)
+import Color exposing (black, white, yellow)
 import Signal exposing ((<~), (~))
 import Time exposing (Time, fps, inSeconds)
 import Keyboard
+import Window
 import List exposing (map)
+import Debug
 
 --Inputs to the game.
 type alias Input =
@@ -39,10 +44,10 @@ type alias Object a =
 type Direction = Left | Right
 
 type alias Board =
-  Object { numberOfDarts : Int, direction : Direction }
+  Object { radius : Float, numberOfDarts : Int, direction : Direction }
 
 type alias Dart =
-  Object {}
+  Object { height : Float, width : Float, radius : Float }
 
 type alias Player =
   Object { darts : List Dart }
@@ -55,24 +60,32 @@ type alias Game =
   , player : Player
   }
 
+defaultDart : Dart
+defaultDart =
+  { x = 0
+  , y = -300
+  , vx = 0
+  , vy = 0
+  , height = 70
+  , width = 1
+  , radius = 5
+  }
+
 defaultGame : Game
 defaultGame =
   { state = Pause
   , board = { x = 0,
-              y = 0,
-              vx = 1,
-              vy = 1,
+              y = 130,
+              vx = 0,
+              vy = 0,
+              radius = 100,
               numberOfDarts = 0,
               direction = Left
             }
-  , player = { x = 0, y = 10, vx = 0, vy = 0, darts = [] }
+  , player = { x = 0, y = 0, vx = 0, vy = 0, darts = [defaultDart] }
   }
 
 --Update the game.
-collidedWithBoard : Dart -> Board -> Bool
-collidedWithBoard dart board =
-  board.y == dart.y
-
 stepObject : Time -> Object a -> Object a
 stepObject time ({x, y, vx, vy} as object) =
   { object |
@@ -80,12 +93,16 @@ stepObject time ({x, y, vx, vy} as object) =
       y <- y + vy * time
   }
 
+collidedWithBoard : Dart -> Board -> Bool
+collidedWithBoard dart board =
+  dart.y > (board.y - (board.radius + (dart.height / 2) + dart.radius))
+
 stepDart : Time -> Dart -> Board -> Dart
 stepDart time ({x, y, vx, vy} as dart) board =
-  let dart' = stepObject time {dart | vy <- 20}
-      y = collidedWithBoard dart board
+  let dart' = stepObject time {dart | vy <- 400}
+      y' = if collidedWithBoard dart board then dart.y else dart'.y
   in
-     dart'
+     {dart' | y <- y'}
 
 stepGame : Input -> Game -> Game
 stepGame input game =
@@ -93,13 +110,52 @@ stepGame input game =
     {space, delta} = input
     {state, board, player} = game
 
-    state' = if space then Play else Pause
-    darts' = List.map (\dart -> stepDart delta dart board) player.darts
+    state' =
+      if | space -> if state == Play then Pause else Play
+         | otherwise -> state
+
+    darts' =
+      if state == Play
+         then List.map (\dart -> stepDart delta dart board) player.darts
+         else player.darts
+
     player' = {player | darts <- darts'}
+
+    d = Debug.watch "Darts" darts'
+    b = Debug.watch "Board" board
+    s = Debug.watch "State" state'
   in
      {game | state <- state', player <- player'}
 
 gameState : Signal Game
 gameState = Signal.foldp stepGame defaultGame input
 
-main = show defaultGame
+-- View for the game.
+displayBackground : Int -> Int -> Form
+displayBackground width height =
+  filled yellow (rect (toFloat width) (toFloat height))
+
+displayBoard : Board -> Form
+displayBoard board =
+  move (board.x, board.y) (filled black (circle board.radius))
+
+drawDart : Dart -> Form
+drawDart dart =
+    group
+      [ (moveY -(dart.height / 2) <| filled white <| circle dart.radius)
+      , (filled white <| rect dart.width dart.height)
+      ]
+
+displayDart : Dart -> Form
+displayDart dart = move (dart.x, dart.y) (drawDart dart)
+
+display : (Int, Int) -> Game -> Element
+display (width, height) {state, board, player} =
+  container width height middle
+    <| collage width height
+      <| [ displayBackground width height
+         , displayBoard board
+         ]
+         ++ List.map displayDart player.darts
+
+main = display <~ Window.dimensions ~ gameState
